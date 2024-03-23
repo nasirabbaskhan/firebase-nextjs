9feb12150c22b94462ef0e73f510a19129446eb6
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-03-23 02:56:00 -0700

    changes done

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ import React, { useEffect, useState } from "react";
 export default function TodoApp() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [course, setCourse] = useState("");
   const [email, setEmail] = useState("");
   const [student, setStudent] = useState<any[]>([]);
   const [isLooading, setIsLoading] = useState(false);
@@ -23,6 +24,7 @@ export default function TodoApp() {
       const docRef = await addDoc(collection(db, "users"), {
         firstName: firstName,
         lastName: lastName,
+        course: course,
         email: email,
       });
       console.log("Document written with ID: ", docRef.id);
@@ -74,42 +76,55 @@ export default function TodoApp() {
   };
   return (
     <>
-      <div className="flex flex-col gap-3 max-w-96 mx-auto mt-9">
+      <div className="flex flex-col gap-3  mt-9 bg-slate-300 px-5 py-5 max-w-[600px] mx-auto rounded-md shadow-lg">
+        <div className="text-2xl">Student Form</div>
         <input
           onChange={(e) => setFirstName(e.target.value)}
-          className="outline-none border border-black rounded-md p-1 "
+          className="outline-none border border-black rounded-md p-1 mt-3 bg-slate-100 "
           type="text"
           placeholder="Inter your first name"
         />
+
         <input
           onChange={(e) => setLastName(e.target.value)}
-          className="outline-none border border-black rounded-md p-1"
+          className="outline-none border border-black rounded-md p-1 mt-3 bg-slate-100"
           type="text"
           placeholder="Inter your last name"
         />
         <input
+          onChange={(e) => setCourse(e.target.value)}
+          className="outline-none border border-black rounded-md p-1 mt-3 bg-slate-100"
+          type="text"
+          placeholder="Inter your Course"
+        />
+        <input
           onChange={(e) => setEmail(e.target.value)}
-          className="outline-none border border-black rounded-md p-1"
+          className="outline-none border border-black rounded-md p-1 mt-3 bg-slate-100"
           type="email"
           placeholder="Inter your email"
         />
         <button
           onClick={addDocumentHandler}
-          className="bg-slate-600 py-2 px-3 text-white"
+          className="bg-slate-600 py-2 px-3 text-white mt-5"
         >
-          Add Documents
+          Submit Form
         </button>
-        <div>firstName:{firstName}</div>
-        <div>lastName:{lastName}</div>
-        <div>email:{email}</div>
+        <div>firstName: {firstName}</div>
+        <div>lastName: {lastName}</div>
+        <div>Course: {course}</div>
+        <div>email: {email}</div>
       </div>
-      <div className="max-w-[400px] mx-auto mt-10">
-        <div className="text-3xl font-bold text-blue-800">Student Data</div>
-        <table>
+
+      <div className="max-w-[900px] mx-auto mt-10 bg-slate-300 p-4 rounded-md shadow-lg mb-4">
+        <div className="text-3xl font-bold text-blue-800 text-center mb-6">
+          Student Data
+        </div>
+        <table className="mx-auto">
           <thead>
             <tr>
               <th className="border  border-blue-950 px-5 py-2">FirstName</th>
               <th className="border  border-blue-950 px-5 py-2">LastName</th>
+              <th className="border  border-blue-950 px-5 py-2">Course</th>
               <th className="border  border-blue-950 px-5 py-2">Gemail</th>
               <th className="border  border-blue-950 px-5 py-2">Action</th>
             </tr>
@@ -123,6 +138,9 @@ export default function TodoApp() {
                   </td>
                   <td className="border  border-blue-950 px-5 py-2">
                     {item.lastName}
+                  </td>
+                  <td className="border  border-blue-950 px-5 py-2">
+                    {item.course}
                   </td>
                   <td className="border  border-blue-950 px-5 py-2">
                     {item.email}
